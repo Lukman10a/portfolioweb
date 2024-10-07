be6b0a5e58a78993a45e4cb0e43b110c9c924ea5
Lukman10a <abdulrauflukman9@gmail.com>
2024-10-07 20:53:42 +0100

    first commit

---
+++ data.tsx
@@ -0,0 +1,41 @@
+// import img from "./public/images/img.png";
+// import img2 from "./public/images/img2.png";
+
+export const projects = [
+  {
+    title: "Richard Gaston",
+    img: "/images/img.png",
+  },
+  {
+    title: "KangHee Kim",
+    img: "/images/img2.png",
+  },
+  {
+    title: "Richard Gaston",
+    img: "/images/img.png",
+  },
+  {
+    title: "KangHee Kim",
+    img: "/images/img2.png",
+  },
+  // {
+  //   title: "Inka and Niclas",
+  //   img: img,
+  // },
+  // {
+  //   title: "Arch McLeish",
+  //   img: img,
+  // },
+  // {
+  //   title: "Nadir Bucan",
+  //   img: img,
+  // },
+  // {
+  //   title: "Chandler Bondurant",
+  //   img: img,
+  // },
+  // {
+  //   title: "Arianna Lago",
+  //   img: img,
+  // },
+];
